1d67b0acde56112f487f06058361d2c03d8b7852
Jonathan Edward <jonathan.edward.1112049@gmail.com>
2021-04-20 13:52:18 +0700

    Rename main from greeting to main

--- src/test/java/com/invimp/quarkus/NativeMainIT.java
+++ src/test/java/com/invimp/quarkus/NativeMainIT.java
@@ -3,7 +3,7 @@ package com.invimp.quarkus;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeMainIT extends MainTest {
 
     // Execute the same tests but in native mode.
 }
