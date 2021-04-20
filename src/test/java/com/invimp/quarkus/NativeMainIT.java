251bd4c652555aa798be07e43ffa6af6cb581bd2
Jonathan Edward <jonathan.edward.1112049@gmail.com>
2021-04-20 14:38:36 +0700

    Delete unused files

--- src/test/java/com/invimp/quarkus/NativeMainIT.java
+++
@@ -1,9 +0,0 @@
-package com.invimp.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMainIT extends MainTest {
-
-    // Execute the same tests but in native mode.
-}
