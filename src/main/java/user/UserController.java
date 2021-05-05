91ea4718f52449dcf25a772c19d27268575d4726
Jonathan Edward <jonathan.edward.1112049@gmail.com>
2021-05-05 14:28:53 +0700

    Create user service, repository and controller

---
+++ src/main/java/user/UserController.java
@@ -0,0 +1,38 @@
+package user;
+
+import user.model.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static javax.ws.rs.core.Response.noContent;
+import static javax.ws.rs.core.Response.ok;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GET
+    public Response getUser() {
+        List<User> userList = userService.getUsers();
+
+        if(userList.isEmpty()) return noContent().build();
+
+        return ok(userList).build();
+    }
+
+    @POST
+    public Response addUser(User user) {
+        userService.createUser(user);
+
+        return ok(user).build();
+    }
+}
